0003e713137f6eafc1951c277313f0d476e2d184
vorob <pavorobev@edu.aau.at>
2024-03-25 15:46:08 +0100

    add @EqualsAndHashCode @NoArgsConstructor lombok for comparing users, create constructor without long id

--- src/main/java/com/example/demo/appuser/AppUser.java
+++ src/main/java/com/example/demo/appuser/AppUser.java
@@ -1,10 +1,22 @@
 package com.example.demo.appuser;
 
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.Collections;
 
+@Getter
+@Setter
+@EqualsAndHashCode // equals or not users
+@NoArgsConstructor
+@Entity
 
 public class AppUser implements UserDetails {
 
@@ -12,40 +24,57 @@ public class AppUser implements UserDetails {
     private String name;
     private String userName;
     private String email;
+
+    // constructor
+    public AppUser(String name, String userName, String email, String password, AppUserRole appUserRole, Boolean locked, Boolean enabled) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
     private String password;
+    private AppUserRole appUserRole;
+    private Boolean locked;
+    private Boolean enabled;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+
+        return Collections.singletonList(authority);
+    } // the user will have a single authority based on their role, and this authority can be used by Spring Security to control access to resources within the application.
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return !locked;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return enabled;
     }
 }
